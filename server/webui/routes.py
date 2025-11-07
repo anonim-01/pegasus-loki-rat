91d812dffc7cc791d12aee89295cc53676f78c4a
anonim-01 <openhands@all-hands.dev>
2025-11-07 08:32:34 +0000

    okidir
    Signed-off-by: anonim-01 <openhands@all-hands.dev>

--- server/webui/routes.py
+++ server/webui/routes.py
@@ -2,6 +2,14 @@ from flask import render_template
 from . import webui, require_admin
 from ..models import Smartphone, Backup
 
+
+import logging
+
+@webui.route('/login', methods=['GET', 'POST'])
+def login():
+    logging.warning('ACCESSING LOGIN')
+    return render_template('login.html')
+
 @webui.route('/smartphones')
 @require_admin
 def smartphone_list():
@@ -13,8 +21,4 @@ def smartphone_list():
 def backup_list():
     backups = Backup.query.all()
     return render_template('backups.html', backups=backups)
-
-@webui.route('/test')
-def test_route():
-    return 'test'
 
